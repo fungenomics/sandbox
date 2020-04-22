dbc507d80cee4cb2d02d950bbe8e2f4f23be6ad7
sjessa <selinjessa@gmail.com>
2020-04-22 17:46:31 -0400

    Add example test

---
+++ tests/testthat/test-hello.R
@@ -0,0 +1,9 @@
+test_that("hello_world works", {
+  expect_equal(hello_world(4), "hello world!!!!")
+})
+
+
+# An example test to handle an edge case
+# test_that("hello_world handles edge cases", {
+#     expect_error(hello_world(-4), "Please use a positive number")
+# })
