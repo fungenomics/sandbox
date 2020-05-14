3ce4e5ae7f52a3dfd4d8c0cd0f06bedb14f08172
sjessa <selinjessa@gmail.com>
2020-05-13 22:59:14 -0400

    Add the fibonacci_mat dataset

--- R/data.R
+++ R/data.R
@@ -6,3 +6,12 @@
 #'
 #' @format Numeric vector with 9 elements
 "fibonacci"
+
+
+#' Matrix of values to use as test data
+#'
+#' A matrix, where column n contains a random sample from a normal
+#' distribution where the mean is the nth element of the fibonacci vector
+#'
+#' @format Matrix with 100 rows and 9 columns
+"fibonacci_mat"
